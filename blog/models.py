b6150a9a923a8f088391ffff074c784bb83a0804
kasra <you@example.comkasra.taghipoor@gmail.com>
2026-08-09 10:13:21 -0700

    s2 finall 6

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,12 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+class Coment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    body = models.TextField()
+    is_validate = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+            return self.body[:20]
+    
